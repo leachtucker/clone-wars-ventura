ab49e0adeedaf47c1962cecb5420c8c2471968b2
Tucker Leach <leachtucker@gmail.com>
2023-06-05 18:19:59 -0700

    fix: :technologist: Disable xstate devtools within prod build

--- src/shared/providers/GlobalServicesProvider.tsx
+++ src/shared/providers/GlobalServicesProvider.tsx
@@ -4,11 +4,11 @@ import { DesktopMachine, desktopMachine } from '../../machines/desktop.machine';
 import { InterpreterFrom } from 'xstate';
 import { inspect } from '@xstate/inspect';
 
-inspect({
-  // options
-  // url: 'https://stately.ai/viz?inspect', // (default)
-  iframe: false, // open in new window
-});
+if (import.meta.env.DEV) {
+  inspect({
+    iframe: false, // open in new window
+  });
+}
 
 type Context = {
   desktopService: InterpreterFrom<DesktopMachine>;
